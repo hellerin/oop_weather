09348cc2e6c137cd604e0c5fb7e9a5e7d86d7676
Hellerin <hellerinm@gmail.com>
2021-12-15 15:26:13 +0200

    Change weather data from API

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const weather = new Weather("Tallinn")
 
 //UI object
 const ui = new UI()
+
+//Events
+const form = document.querySelector("#change-city")
+form.addEventListener("submit", changeWeather)
+
+//Change weather
+function changeWeather(event){
+    const city = document.querySelector("#city-name").value
+    weather.changeCity(city)
+    getWeather()
+    document.querySelector("#city-name").value = ""
+    event.preventDefault()
+}
+
 //Get city weather
 function getWeather(){
     weather.getWeather()
